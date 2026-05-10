7404a2ae6b408841fd2e0df817524be219773be3
lauracarrillorodriguez74-creator <laura.carrillo.rodriguez74@gmail.com>
2026-05-10 19:27:30 +0200

    Update app.jsx

--- APP/app.jsx
+++ APP/app.jsx
@@ -230,22 +230,27 @@ function TopBar({ active, onCreate, currentUser, onHamburger, onSwitchUser }) {
       </button>
 
       <button className="tf-btn tf-btn-pri" onClick={onCreate}>
-        <button
+  <Icon name="plus" size={14}/>
+  <span className="tf-btn-txt">Nueva tarea</span>
+</button>
+
+<button
   className="tf-btn"
   onClick={async () => {
     const supa = window.__supabase || window.tfSupabase;
 
-    await supa.auth.signOut();
+    if (supa) {
+      await supa.auth.signOut();
+    }
 
     localStorage.clear();
+    sessionStorage.clear();
 
     window.location.reload();
   }}
 >
   Salir
 </button>
-        <Icon name="plus" size={14}/><span className="tf-btn-txt">Nueva tarea</span>
-      </button>
 
       {/* Avatar en mobile */}
       <div style={{ display:"none" }} id="tf-mob-av">
